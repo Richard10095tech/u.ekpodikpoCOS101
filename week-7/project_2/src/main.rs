e72cba1fffa85dcce3319248c41f26e9279f92dc
Richard10095tech <uyohoini.ekpodikpo@pau.edu.ng>
2024-12-10 16:33:11 +0100

    (addition to my seventh commit)

--- week-7/project_2/src/main.rs
+++ week-7/project_2/src/main.rs
@@ -4,18 +4,46 @@ use std::io;
 
 fn checker(){
     let mut input = String::new();
-    println!("Enter your years of experience");
+    println!("Welcome! how many programers do you want to interview");
     io::stdin().read_line(&mut input).expect("Failed to read input");
-    let _years:i32 = input.trim().parse().expect("Invalid input");
+    let _number:i32 = input.trim().parse().expect("Invalid input");
 
-    if _years > 10 // you have more than 10 years experience
+    if _number == 3 // you want to interview the first programers
     {
-        println!("You can be selected as the programer with the highest years of experience");
-    }
-    else {
-        println!("You can never be selected as the programer with the highest years of experience");
-    }
+        println!("Enter the name of first programer");
+        io::stdin().read_line(&mut input).expect("Failed to read input");
+        let _number:String= input.trim().parse().expect("Invalid input");
 
+        println!("Enter the amount of years of experience");
+        io::stdin().read_line(&mut input).expect("Failed to read input");
+        let _number:i32 = input.trim().parse().expect("Invalid input");
+
+    } 
+        if _number == 3 // you to interview the second programer
+        {
+        println!("Enter the name of the second programer");
+        io::stdin().read_line(&mut input).expect("Failed to read input");
+        let _number:String = input.trim().parse().expect("Invalid input");
+
+        println!("Enter the amount of years  of experience");
+        io::stdin().read_line(&mut input).expect("Failed to read input");
+        let _number:i32 = input.trim().parse().expect("Invalid input");
+        }
+
+         if _number == 3 // you to interview the third programer
+        {
+        println!("Enter the name of the third programer");
+        io::stdin().read_line(&mut input).expect("Failed to read input");
+        let _number:String = input.trim().parse().expect("Invalid input");
+
+        println!("Enter the amount of years  of experience");
+        io::stdin().read_line(&mut input).expect("Failed to read input");
+        let _number:i32 = input.trim().parse().expect("Invalid input");
+        }
+
+        else {
+            println!("If you are selected as the programer with the highest years of experience, you are quqlified for the job");
+        }
 }
 
 fn main() {
--- week-8/practice_1/src/main.rs
+++ week-8/practice_1/src/main.rs
@@ -1,3 +1,15 @@
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+
+    let announce = "Week 8 - Rust File Input & Output\n";
+    let dept = "Department of Computer Science";
+
+    let mut file = std::fs::File::create("data.txt").expect("create failed");
+    file.write_all("Welcome to Rust Programming\n".as_bytes()).expect("write failed");
+    file.write_all(announce.as_bytes()).expect("write failed");
+    file.write_all(dept.as_bytes()).expect("write failed");
+    println!("\nData written to file." );
+
 }
+
